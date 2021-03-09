947e016eaee2902576756786ce907f216c2ce330
Charli <charli@kompjuter.dk>
2021-03-09 11:35:25 +0100

    seperated genres with , instead of -

--- src/components/movies/MovieBackgroundImage.js
+++ src/components/movies/MovieBackgroundImage.js
@@ -79,7 +79,7 @@ const MovieBackgroundImage = ({ movie, image, danishTitle, genre }) => {
       </SharedElement>
       
       <Text style={styles.movieGenre} numberOfLines={2}>
-        {genreFallback.join(' - ')} {"\n"}{ movie.playingTime ? `Varighed ${movie.playingTime} min` : null}
+        {genreFallback.join(', ')} {"\n"}{ movie.playingTime ? `Varighed ${movie.playingTime} min` : null}
       </Text>
     </View>
     
