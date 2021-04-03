3c45935eb9ec0695accc94aa4eb597ce90239d05
Al2gether22 <wj2005034@gmail.com>
2021-04-04 01:30:41 +0200

    Fix for cinema sorting

--- src/context/CinemaContext.js
+++ src/context/CinemaContext.js
@@ -1,62 +1,63 @@
-import dataContext from "./DataContext";
-import _ from "lodash";
-import { computeDistance } from "../helpers/computeDistance";
-
+import dataContext from './DataContext';
+import _ from 'lodash';
+import {computeDistance} from '../helpers/computeDistance';
 
 const cinemaReducer = (state, action) => {
   switch (action.type) {
-    case "add_error":
-      return { ...state, errorMessage: action.payload };
-    case "get_cinemas":
-      return { ...state, cinemas: action.payload };
-    case "update_cinemas":
-      return { ...state, cinemas: action.payload, cinemasSorted: true };
+    case 'add_error':
+      return {...state, errorMessage: action.payload};
+    case 'get_cinemas':
+      return {...state, cinemas: action.payload, cinemasFetched: true};
+    case 'update_cinemas':
+      return {...state, cinemas: action.payload, cinemasSorted: true};
     default:
-      return state
+      return state;
   }
 };
 
 const getCinemas = dispatch => async () => {
   try {
-    const response = await fetch(
-      "https://www.kino.dk/appservices/cinemas",
-      { mode: "no-cors" })
+    const response = await fetch('https://www.kino.dk/appservices/cinemas', {
+      mode: 'no-cors',
+    });
     const cinemas = await response.json();
-    console.log("Get cinemas called from context")
+    console.log('Get cinemas called from context');
     dispatch({
-      type: "get_cinemas",
-      payload: cinemas
+      type: 'get_cinemas',
+      payload: cinemas,
     });
   } catch (err) {
     dispatch({
-      type: "add_error",
-      payload: "Something went wrong with the cinemas"
-    })
+      type: 'add_error',
+      payload: 'Something went wrong with the cinemas',
+    });
   }
-}
+};
 
-const updateCinemas = (dispatch) => {
-
+const updateCinemas = dispatch => {
   return async (cinemas, referenceLat, referenceLong) => {
-    
     const cinemasWithDistance = cinemas.map(cinema => {
       return {
         ...cinema,
-        distance: computeDistance([cinema.geo.latitude, cinema.geo.longitude], [referenceLat, referenceLong]) // Calculate the distance
+        distance: computeDistance(
+          [cinema.geo.latitude, cinema.geo.longitude],
+          [referenceLat, referenceLong],
+        ), // Calculate the distance
       };
-    });    
+    });
     const orderedCinemas = _.orderBy(cinemasWithDistance, 'distance');
-    console.log("Update Cinemas called from context")
-    dispatch({ type: "update_cinemas", payload: orderedCinemas });
+    console.log('Update Cinemas called from context');
+    console.log('orderedCinemas first item', orderedCinemas[0]);
+    dispatch({type: 'update_cinemas', payload: orderedCinemas});
     // Update cinemas is called with cinemas array and user coordinates
     // A distance value is added to each cinema based on user coords and cinema cords
     // cinemas are sorted by distance
     // state is updated with new cinemas array and a value of cinemaSorted to be true
-  }
-}
+  };
+};
 
-export const { Context, Provider } = dataContext(
+export const {Context, Provider} = dataContext(
   cinemaReducer,
-  { updateCinemas, getCinemas },
-  { cinemas: [], errorMessage: '', cinemasSorted: false }
+  {updateCinemas, getCinemas},
+  {cinemas: [], errorMessage: '', cinemasSorted: false, cinemasFetched: false},
 );
