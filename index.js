310e98a5a123eaebf5f1d06e08c0a3f93ef0e721
Al2gether22 <wj2005034@gmail.com>
2021-04-12 20:37:29 +0200

    Ignore file not found warnign

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Could not find image file']); // Ignore log notification by message
 
 AppRegistry.registerComponent(appName, () => App);
