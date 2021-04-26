4db734ada7da59a95aeb6a569c0a6211798113d6
Al2gether22 <wj2005034@gmail.com>
2021-04-26 04:12:34 +0200

    Fixed crash on cinama screen in android

--- src/screens/Cinema.js
+++ src/screens/Cinema.js
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, StatusBar } from "react-native"
-import CinemaBackgroundImage from "../components/cinemas/CinemaBackgroundImage"
-import CinemaMetaData from "../components/cinemas/CinemaMetaData"
-import ShowTimes from "../components/cinemas/ShowTimes";
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, StatusBar, Text} from 'react-native';
+import CinemaBackgroundImage from '../components/cinemas/CinemaBackgroundImage';
+import CinemaMetaData from '../components/cinemas/CinemaMetaData';
+import ShowTimes from '../components/cinemas/ShowTimes';
 import GestureRecognizer from 'react-native-swipe-gestures';
-import { useNavigation } from "@react-navigation/native";
+import {useNavigation} from '@react-navigation/native';
+import {Platform} from 'react-native';
 
-
-const Cinema = ({ route }) => {
-  const { item } = route.params;
-  const [cinema, setCinema] = useState([])
-  const [loading, setLoading] = useState(true)
+const Cinema = ({route}) => {
+  const {item} = route.params;
+  const [cinema, setCinema] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
   const config = {
     velocityThreshold: 0.8,
     directionalOffsetThreshold: 150,
-    gestureIsClickThreshold: 10
+    gestureIsClickThreshold: 10,
   };
 
   // fetches cinema data
   useEffect(() => {
     fetch(`https://www.kino.dk/appservices/cinema/${item.id}`, {
-      mode: "no-cors"
-      })
-      .then((response) => response.json())
-      .then((json) => setCinema(json))
-      .catch((error) => console.error(error))
+      mode: 'no-cors',
+    })
+      .then(response => response.json())
+      .then(json => setCinema(json))
+      .catch(error => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
@@ -34,34 +34,27 @@ const Cinema = ({ route }) => {
       <FlatList
         keyboardShouldPersistTaps="always"
         style={styles.container}
-        keyExtractor={(index) => index.toString()}
+        keyExtractor={index => index.toString()}
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={
-          <GestureRecognizer      
+          <GestureRecognizer
             onSwipeDown={() => navigation.goBack()}
             config={config}
             style={{
               flex: 1,
-              backgroundColor: "transparent",
-              width: "100%",
-              zIndex: 999999
-            }}
-          >
+              backgroundColor: 'transparent',
+              width: '100%',
+              zIndex: 999999,
+            }}>
             <StatusBar hidden={true} />
-            <CinemaBackgroundImage
-              name={item.name}
-              img={item.imageUrl}
-            />
-            { loading ? null : <CinemaMetaData cinema={cinema} />}
-            
+            <CinemaBackgroundImage name={item.name} img={item.imageUrl} />
+            {loading ? null : <CinemaMetaData cinema={cinema} />}
           </GestureRecognizer>
         }
         ListFooterComponent={
-          loading ? null : 
-          <ShowTimes 
-            id={cinema.nid} 
-            movieVersions={item.versions}
-          />
+          loading ? null : (
+            <ShowTimes id={cinema.nid} movieVersions={item.versions} />
+          )
         }
       />
     </>
@@ -71,27 +64,38 @@ const Cinema = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#1d1d27",
+    flexDirection: 'column',
+    backgroundColor: '#1d1d27',
   },
 });
 
 Cinema.sharedElements = route => {
-  const { item } = route.params;
-  
+  const {item} = route.params;
+
+  //For some reason android crashes with React.children error
+  //if we return an array here, hence this below
+  if (Platform.OS === 'android') {
+    return {
+      id: item.imageUrl,
+      animation: 'fade',
+      resize: 'clip',
+      align: 'auto',
+    };
+  }
+
   return [
     {
-      id: item.imageUrl,
-      animation: "fade",
-      resize: "clip", 
-      align: "auto"
-    }, 
-    {
       id: item.name,
-      animation: "fade",
-      resize: "none", 
-      align: "auto"
-    }
+      animation: 'fade',
+      resize: 'none',
+      align: 'auto',
+    },
+    {
+      id: item.imageUrl,
+      animation: 'fade',
+      resize: 'clip',
+      align: 'auto',
+    },
   ];
 };
 
