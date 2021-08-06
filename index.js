d49577a5f7f67e13ffca61f2bf4e92cffa001f69
Al2gether22 <wj2005034@gmail.com>
2021-08-06 02:49:19 -0500

    Added codepush wrapper to app.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {LogBox} from 'react-native';
+
 LogBox.ignoreLogs([
   'Could not find image file',
   'Possible Unhandled Promise Rejection',
